14803898f848ed909a608226fa9cb41b3b56f14b
Adil Imran <devadem1370@gmail.com>
2025-06-25 22:17:41 -0700

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
+import Navbar from './components/Navbar'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 
 const App = () => {
   return (
-    <div className='bg-red-500'>App</div>
-  )
+<main>
+  <Navbar/>
+</main>  )
 }
 
 export default App
